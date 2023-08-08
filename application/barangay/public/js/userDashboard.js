da10672eec132e39350c8acf4a62c4df7aa64cd8
jake-xxi <jakersonbermudo98@gmail.com>
2023-08-08 22:46:45 +0800

    DEL BARANGAY FOLDER

--- application/barangay/public/js/userDashboard.js
+++
@@ -1,8 +0,0 @@
-function clock() {
-    const timeDisplay = document.getElementById("clock");
-    const dateString = new Date().toLocaleString();
-    const formattedString = dateString.replace(", ", " - ");
-    timeDisplay.textContent = formattedString;
-  }
-
-  setInterval(clock, 1000);
